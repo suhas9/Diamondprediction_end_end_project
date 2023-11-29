a16c61d9f94e6e5d7aabb645dd13669605a1f487
suhas9 <suhassaligrama99@gmail.com>
2023-11-29 12:00:24 +0530

    created setup file

--- init_setup.sh
+++ init_setup.sh
@@ -2,7 +2,7 @@ echo [$(date)]: "Start"
 
 echo [$(date)]: "creating env with python 3.8 version"
 
-conda create --prefix ./env python=3.8 -y
+conda create -p ./env python=3.8 -y
 
 echo [$(date)]: "activating the envinorment"
 
