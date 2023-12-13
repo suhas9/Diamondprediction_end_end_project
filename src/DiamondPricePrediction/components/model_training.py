538c68adb31a59a8f3f223a7e0bd9a98e3d7d5fc
suhas9 <suhassaligrama99@gmail.com>
2023-12-13 13:13:23 +0530

    model trained

--- src/DiamondPricePrediction/components/model_training.py
+++ src/DiamondPricePrediction/components/model_training.py
@@ -1,6 +1,64 @@
+import os,sys
+import pandas as pd
+import numpy as np
+
+from dataclasses import dataclass
+from src.DiamondPricePrediction.logger import logging
+from src.DiamondPricePrediction.exception import CustomException
+
+from dataclasses import dataclass
+from src.DiamondPricePrediction.utils.utils import save_object
+from src.DiamondPricePrediction.utils.utils import evalute_model
+
+from sklearn.linear_model import LinearRegression,Ridge,Lasso,ElasticNet
+
+@dataclass
+class ModelTrainingConfig:
+    trained_model_file_path = os,path.join('Artifacts','model.pkl')
+
+
+
 class ModelTrainer:
     def __init__(self):
-        pass
+       self.model_trainer_config = ModelTrainingConfig()
 
     def Initiate_model_trainer(self):
-        pass
+        try:
+            logging.info('Splitting Dependent and Independent variable from train and test data')
+            X_train, y_train,X_test,,y_test = (
+                train_array[:,:-1],
+                train_array = [:,:-1],
+                test_array = [:,:-1],
+                test_array = [:,:-1]
+            )
+            models = {
+                'LinearRegression':LinearRegression(),
+                'Lasso':Lasso(),
+                'Ridge':Ridge(),
+                'ElasticNet':ElasticNet()
+            }
+
+            model_report:dict = evalute_model(X_train, y_train,X_test,,y_test,models)
+            print(model_report)
+            print('\n===============================================================\n')
+            logging.info(f'model report:{model_report}')
+
+            #to get best model score from dictionary
+            best_model_score = max(sorted(model_report.values()))
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+
+            best_model = models[best_model_name]
+            print(f'Best model found,Model Name:{best_model_name},R2 score:{best_model_score}')
+            print('\n=========================================================================\n')
+            logging.info(f'Best model found ,Model name:{best_model_name},R2 score:{best_model_score}')
+
+            save_object(
+                file_path = self.model_trainer_config.trained_data_path,
+                obj = best_model
+            )
+
+
+        except Exception as e:
+            raise CustomException(e,sys)
